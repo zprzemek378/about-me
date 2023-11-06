a6d69f7c83a965f07f63ad861f62cc7cabb67a29
zprzemek378 <zprzemek378@gmail.com>
2023-11-06 17:08:12 +0100

    website 5:08PM

--- src/components/MyProjectsSection/MyProjectsSection.tsx
+++ src/components/MyProjectsSection/MyProjectsSection.tsx
@@ -44,13 +44,13 @@ const MyProjectsSection = () => {
           <li>Integration of user reviews and ratings for trustworthiness.</li>
         </div>
         <img
-          src="about-me/freshrental2.png"
+          src="%PUBLIC_URL%/freshrental2.png"
           alt="freshRental2"
           className="freshRental"
         />
       </div>
       <div className="text-center m-5 text-xl">
-        Explore my{" "}
+        Explore myyyy{" "}
         <a
           className="font-bold text-blue-800"
           href="https://zprzemek378.github.io/fresh-car-rental"
